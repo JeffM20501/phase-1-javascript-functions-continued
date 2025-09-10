c8b8d1474d30af661fdcb251a86eef4c5a80ce14
JeffM20501 <jeff.muna@student.moringaschool.com>
2025-09-10 14:21:37 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity=null){
+    return `This Saturday, I want to ${activity?activity:'roller-skate'}!`
+}
+
+console.log(saturdayFun("bathe my dog"))
+
+const mondayWork=function(activity=null){
+    return `This Monday, I will ${activity?activity:"go to the office"}.`
+}
+
+const wrapAdjective=(flair="*")=>{
+   return function(statement="Special"){
+    return `You are ${flair}${statement}${flair}!`
+   }
+}
